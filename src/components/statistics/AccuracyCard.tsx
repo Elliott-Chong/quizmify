14b629f478b26633253c2a31bf67d5088b20197b
Elliott Chong <elliottchong16@gmail.com>
2023-07-23 12:26:15 +0800

    accuracy box

--- src/components/statistics/AccuracyCard.tsx
+++ src/components/statistics/AccuracyCard.tsx
@@ -5,6 +5,7 @@ import { Target } from "lucide-react";
 type Props = { accuracy: number };
 
 const AccuracyCard = ({ accuracy }: Props) => {
+  accuracy = Math.round(accuracy * 100) / 100;
   return (
     <Card className="md:col-span-3">
       <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
